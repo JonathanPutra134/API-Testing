86ab0620b28fdfa0894f8e9eb1bf70c935da5d36
GenshinimpactJokijo <jimmywangkar@gmail.com>
2026-03-01 18:19:58 +0700

    add ping endpoint

--- main.go
+++ main.go
@@ -13,21 +13,25 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	api := e.Group("/api")
-	v1 := api.Group("/v1")
-	v1.GET("/hello", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{"message": "hello world"})
-	})
+	e.GET("/ping", ping)
+	// e.POST("/echo", notImplemented)
+	// e.POST("/books", notImplemented)
+	// e.GET("/books", notImplemented)
+	// e.GET("/books/:id", notImplemented)
+	// e.PUT("/books/:id", notImplemented)
+	// e.DELETE("/books/:id", notImplemented)
+	// e.POST("/auth/token", notImplemented)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	e.Logger.Fatal(e.Start(":" + port))
-}
 
-func helloHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "hello world",
+	e.Logger.Fatal(e.Start(":" + port))
+}		
+
+func ping(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]bool{
+		"success": true,
 	})
-}
+}	
